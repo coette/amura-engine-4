a300b4cc8063dd981eaec851673fb0a1ddab771d
coette <124669272+coette@users.noreply.github.com>
2026-08-21 12:02:57 +0200

    R1.5 cache-bust watch entry

--- wrist-watch.js
+++ wrist-watch.js
@@ -1,1 +1,1 @@
-export { updateWristWatch, holdWristWatch, hideWristWatch } from "./engine4-watch-entry.js?v=e4r1.4";
+export { updateWristWatch, holdWristWatch, hideWristWatch } from "./engine4-watch-entry.js?v=e4r1.5";
